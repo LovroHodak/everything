36df3e44befec69359138733afcbc37a8f8a9a1b
Lovro <hodak.lovro@gmail.com>
2022-05-30 21:07:22 +0200

    sort()

--- src/components/Second.js
+++ src/components/Second.js
@@ -1,13 +1,60 @@
 import React, { useMemo, useState } from "react";
 import { tw } from "../tailwind";
 
-export default function Second({ members }) {
-  
-  
+export default function Second({ members = [] }) {
+  const [ppl, setPpl] = useState([...members]);
+
+  console.log(members);
+  console.log(ppl);
+  const myFolks = useMemo(() => {
+    return ppl.sort(function (a, b) {
+      const nameA = a.name.toLowerCase();
+      const nameB = b.name.toLowerCase();
+      if (nameA < nameB)
+        //sort string ascending
+        return -1;
+      if (nameA > nameB) return 1;
+      return 0; //default return value (no sorting)
+    });
+  }, [ppl]);
+
+  console.log(myFolks);
+
+  console.log(members);
+
+  const [sortType, setSortType] = useState("");
+
+  const sortedQuery = useMemo(() => {
+    if (!sortType) {
+      return members;
+    } else if (sortType === "nameUp") {
+      return [...ppl].sort(function (a, b) {
+        const nameA = a.name.toLowerCase();
+        const nameB = b.name.toLowerCase();
+        if (nameA < nameB)
+          //sort string ascending
+          return -1;
+        if (nameA > nameB) return 1;
+        return 0; //default return value (no sorting)
+      });
+    } else if (sortType === "nameDown") {
+      return [...ppl].sort(function (a, b) {
+        const nameA = a.name.toLowerCase();
+        const nameB = b.name.toLowerCase();
+        if (nameA > nameB)
+          //sort string ascending
+          return -1;
+        if (nameA < nameB) return 1;
+        return 0; //default return value (no sorting)
+      });
+    }
+  }, [sortType, members, ppl]);
+
   return (
     <SecondContainer>
-      <SortBtn>Name Up</SortBtn>
-      {members.map((member, i) => {
+      <SortBtn onClick={() => setSortType('nameUp')}>Name Up</SortBtn>
+      <SortBtn onClick={() => setSortType('nameDown')}>Name Down</SortBtn>
+      {sortedQuery.map((member, i) => {
         return <p key={i}>{member.name}</p>;
       })}
     </SecondContainer>
@@ -20,4 +67,4 @@ h-60 border-2 border-green-700 p-1 mb-1
 
 const SortBtn = tw.button`
 border-2 border-black
-`
+`;
